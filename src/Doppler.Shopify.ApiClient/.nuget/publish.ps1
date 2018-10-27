1568189db392c4d766c6fa71e0b2c096896388d6
pbarrios <pbarrios@makingsense.com>
2018-10-27 19:20:11 -0300

    WIP

---
+++ src/Doppler.Shopify.ApiClient/.nuget/publish.ps1
@@ -0,0 +1,33 @@
+Param(
+    [string]$assemblyFolder
+)
+
+if(-not($assemblyFolder)) {
+    $assemblyFolder = '..\Doppler.Shopify.ApiClient\bin\Release\Merge'
+}
+$packagesDir = '..\Nuget'
+$assemblyFile = [io.path]::Combine($PSScriptRoot, $assemblyFolder, 'Doppler.Shopify.ApiClient.dll')
+
+cd $PSScriptRoot
+& 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe' ..\Doppler.Shopify.ApiClient\Doppler.Shopify.ApiClient.csproj /p:Configuration=Release /p:Publish=True
+
+if ($LastExitCode -ne 0)
+{
+  Write-Output "Error building Doppler.Shopify.ApiClient library."
+}
+else
+{
+  $assembly = [Reflection.Assembly]::LoadFrom($assemblyFile)
+  $assemblyVersion =  $assembly.GetName().Version.ToString()
+  $packageName = [io.path]::Combine($packagesDir, 'Doppler.Shopify.ApiClient.' + $assemblyVersion + '.nupkg')
+
+  if((Test-Path $packagesDir) -eq 0)
+  {
+      md $packagesDir
+  }
+
+  .\NuGet.exe pack Doppler.Shopify.ApiClient.nuspec -BasePath . -OutputDirectory $packagesDir -Version $assemblyVersion
+  # .\NuGet.exe push $packageName -source https://nuget.dataonix.com/api/v2/package 1234-dataonix -ConfigFile NuGet.Config
+}
+
+read-Host "Pausing..."
---
+++ src/Doppler.Shopify.ApiClient/ShopifySharp/Infrastructure/ShopifyException.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace ShopifySharp
+{
+    public class ShopifyException : Exception
+    {
+        public HttpStatusCode HttpStatusCode { get; set; }
+
+        /// <summary>
+        /// The XRequestId header returned by Shopify. Can be used when working with the Shopify support team to identify the failed request.
+        /// </summary>
+        public string RequestId { get; set; }
+
+        /// <remarks>
+        /// Dictionary is always initialized to ensure null reference errors won't be thrown when trying to check error messages.
+        /// </remarks>
+        public Dictionary<string, IEnumerable<string>> Errors { get; set; }
+
+        /// <summary>
+        /// The raw JSON string returned by Shopify.
+        /// </summary>
+        public string RawBody { get; set; }
+
+        public ShopifyException() 
+        {
+            Errors = new Dictionary<string, IEnumerable<string>>();
+        }
+
+        public ShopifyException(string message) : base(message) 
+        { 
+            Errors = new Dictionary<string, IEnumerable<string>>();
+        }
+
+        public ShopifyException(HttpStatusCode httpStatusCode, Dictionary<string, IEnumerable<string>> errors, string message, string rawBody, string requestId) : base(message)
+        {
+            HttpStatusCode = httpStatusCode;
+            Errors = errors;
+            RawBody = rawBody;
+            RequestId = requestId;
+            Errors = new Dictionary<string, IEnumerable<string>>();
+        }
+    }
+}
